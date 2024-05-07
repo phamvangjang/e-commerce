75d68e95e02aa409e61fcaab2e9e05df4d5110ac
phamvangjang <giangphamvan48@gmail.com>
2024-05-07 19:55:45 +0700

    update: Login with redux toolkit

--- client/src/pages/public/Login.js
+++ client/src/pages/public/Login.js
@@ -1,15 +1,55 @@
 import React, { useState, useCallback } from "react";
 import { InputField, Button } from '../../components'
+import { apiRegister, apiLogin } from "../../apis/user";
+import Swal from 'sweetalert2'
+import { useNavigate } from "react-router-dom";
+import path from "../../ultils/path";
+import { register } from '../../store/user/userSlice';
+import { useDispatch } from "react-redux";
+
 const Login = () => {
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
     const [payload, setPayload] = useState({
         email: '',
         password: '',
-        name: ''
+        firstname: '',
+        lastname: '',
+        mobile: ''
     })
     const [isRegister, setIsRegister] = useState(false)
-    const handleSubmit = useCallback(() => {
+    const resetPayload = () => {
+        setPayload({
+            email: '',
+            password: '',
+            firstname: '',
+            lastname: '',
+            mobile: ''
+        })
+    }
+    const handleSubmit = useCallback(async () => {
+        const { firstname, lastname, mobile, ...data } = payload
+        if (isRegister) {
+            const response = await apiRegister(payload)
+            if (response.success) {
+                Swal.fire('Congratulation', response.mes, 'success').then(() => {
+                    setIsRegister(false)
+                    resetPayload()
+                })
+            } else Swal.fire('Failed', response.mes, 'error')
 
-    }, [payload])
+            // Swal.fire(response.success ? 'Congratulation' : 'Failed', response.mes, response.success ? 'success' : 'error').then(() => {
+            //     setIsRegister(false)
+            //     resetPayload()
+            // })
+        } else {
+            const rs = await apiLogin(data)
+            if (rs.success) {
+                dispatch(register({ isLoggedIn: true, token: rs.accessToken, userData: rs.userData }))
+                navigate(`/${path.HOME}`)
+            } else Swal.fire('Failed', rs.mes, 'error')
+        }
+    }, [payload, isRegister])
     return (
         <div className="w-screen h-screen relative">
             <img
@@ -20,17 +60,31 @@ const Login = () => {
             <div className="absolute top-0 bottom-0 left-0 right-1/2 items-center justify-center flex">
                 <div className="flex flex-col items-center p-8 bg-white rounded-md min-w-[500px]">
                     <h1 className="text-[28px] font-semibold text-main mb-8">{isRegister ? 'Regster' : 'Login'}</h1>
-                    {isRegister && <InputField
-                        value={payload.name}
-                        setValue={setPayload}
-                        nameKey='name'
-                    />}
+                    {isRegister && <div className="flex items-start gap-2">
+                        <InputField
+                            value={payload.firstname}
+                            setValue={setPayload}
+                            nameKey='firstname'
+                        />
+
+                        <InputField
+                            value={payload.lastname}
+                            setValue={setPayload}
+                            nameKey='lastname'
+                        />
+                    </div>}
 
                     <InputField
                         value={payload.email}
                         setValue={setPayload}
                         nameKey='email'
                     />
+
+                    {isRegister && <InputField
+                        value={payload.mobile}
+                        setValue={setPayload}
+                        nameKey='mobile'
+                    />}
 
                     <InputField
                         value={payload.password}
---
+++ client/src/store/user/userSlice.js
@@ -0,0 +1,42 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const userSlice = createSlice({
+    name: 'user',
+    initialState: {
+        isLoggedIn: false,
+        current: null,
+        token: null
+    },
+    reducers: {
+        register: (state, action) => {
+            state.isLoggedIn = action.payload.isLoggedIn
+            state.current = action.payload.userData
+            state.token = action.payload.token
+        }
+    },
+    // Code logic xử lý async action
+    // extraReducers: (builder) => {
+    //     // Bắt đầu thực hiện action login (Promise pending)
+    //     builder.addCase(getNewProducts.pending, (state) => {
+    //         // Bật trạng thái loading
+    //         state.isLoading = true;
+    //     });
+
+    //     // Khi thực hiện action login thành công (Promise fulfilled)
+    //     builder.addCase(getNewProducts.fulfilled, (state, action) => {
+    //         // console.log(action)
+    //         // Tắt trạng thái loading, lưu thông tin user vào store
+    //         state.isLoading = false;
+    //         state.newProducts = action.payload;
+    //     });
+
+    //     // Khi thực hiện action login thất bại (Promise rejected)
+    //     builder.addCase(getNewProducts.rejected, (state, action) => {
+    //         // Tắt trạng thái loading, lưu thông báo lỗi vào store
+    //         state.isLoading = false;
+    //         state.errorMessage = action.payload.message;
+    //     });
+    // },
+})
+export const { register } = userSlice.actions
+export default userSlice.reducer
