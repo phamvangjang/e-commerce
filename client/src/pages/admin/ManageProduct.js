0f808816f509583cbaad9749a9a026f1e96b873e
phamvangjang <giangphamvan48@gmail.com>
2024-05-18 12:25:39 +0700

    update: update product

--- client/src/pages/admin/ManageProduct.js
+++ client/src/pages/admin/ManageProduct.js
@@ -1,10 +1,11 @@
-import React, { memo, useEffect, useState } from 'react'
+import React, { memo, useCallback, useEffect, useState } from 'react'
 import { InputForm, Pagination } from 'components'
 import { useForm } from 'react-hook-form'
 import { apiGetProducts } from 'apis'
 import moment from 'moment'
 import { useSearchParams, createSearchParams, useNavigate, useLocation } from 'react-router-dom'
 import useDebounce from 'hooks/useDebounce'
+import UpdateProduct from './UpdateProduct'
 
 const ManageProduct = () => {
     const navigate = useNavigate()
@@ -12,6 +13,11 @@ const ManageProduct = () => {
     const [params] = useSearchParams()
     const [products, setProducts] = useState(null)
     const [counts, setCounts] = useState(0)
+    const [editProduct, setEditProduct] = useState(null)
+    const [update, setUpdate] = useState(false)
+    const render = useCallback(() => {
+        setUpdate(!update)
+    })
     const { register, formState: { errors }, handleSubmit, reset, watch } = useForm()
     const handleSearchProduct = (data) => {
         console.log(data)
@@ -43,14 +49,22 @@ const ManageProduct = () => {
         const searchParams = Object.fromEntries([...params])
 
         fetchProducts(searchParams)
-    }, [params])
+    }, [params, update])
 
-    console.log(products)
+    // console.log(products)
     return (
         <div className='w-full flex flex-col gap-4 relative'>
+
+            {editProduct && <div className='absolute inset-0 min-h-screen bg-gray-100 z-50'>
+                <UpdateProduct
+                    editProduct={editProduct}
+                    render={render}
+                />
+            </div>}
+
             <div className='h-[70px] w-full'></div>
 
-            <div className='bg-gray-100 p-4 border-b w-full justify-between items-center border-gray-900 fixed'>
+            <div className='bg-gray-100 p-4 border-b w-full justify-between items-center border-gray-900 fixed top-0'>
                 <h1 className='text-3xl font-bold tracking-tight'>Manage Products</h1>
             </div>
 
@@ -83,6 +97,7 @@ const ManageProduct = () => {
                         <th className='border border-gray-500 py-2'>Color</th>
                         <th className='border border-gray-500 py-2'>Rating</th>
                         <th className='border border-gray-500 py-2'>Update At</th>
+                        <th className='border border-gray-500 py-2'>Action</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -107,6 +122,15 @@ const ManageProduct = () => {
                             <td className='border border-gray-500 py-2 text-center'>{el.color}</td>
                             <td className='border border-gray-500 py-2 text-center'>{el.totalRatings}</td>
                             <td className='border border-gray-500 py-2 text-center'>{moment(el.updatedAt).format('DD/MM/YYYY')}</td>
+                            <td className='border border-gray-500 py-2 text-center'>
+                                <span
+                                    onClick={() => setEditProduct(el)}
+                                    className='text-orange-500 hover:underline cursor-pointer px-1'>
+                                    Edit</span>
+                                <span
+                                    className='text-orange-500 hover:underline cursor-pointer px-1'>
+                                    Remove</span>
+                            </td>
                         </tr>
                     ))}
                 </tbody>
---
+++ client/src/pages/admin/UpdateProduct.js
@@ -0,0 +1,267 @@
+import { InputForm, MarkdownEditor, Button, Loading, Select } from 'components'
+import React, { memo, useState, useEffect, useCallback } from 'react'
+import { set, useForm } from 'react-hook-form'
+import { validate, getBase64 } from 'ultils/helpers'
+import { toast } from 'react-toastify'
+import { useDispatch, useSelector } from 'react-redux'
+import { showModel } from 'store/app/appSlice'
+import { apiCreateProduct } from 'apis'
+
+const UpdateProduct = ({ editProduct, render }) => {
+    const { register, handleSubmit, formState: { errors }, reset, watch } = useForm()
+
+    const dispath = useDispatch()
+    const { categories } = useSelector(state => state.app)
+
+    const [preview, setPreview] = useState({
+        thumb: null,
+        images: []
+    })
+
+    const [payload, setPayload] = useState({
+        description: ''
+    })
+
+    const [invalidFields, setInvalidFields] = useState([])
+    const changeValue = useCallback((e) => {
+        setPayload(e)
+    }, [payload])
+
+    const handlePreviewThumb = async (file) => {
+        const base64Thumb = await getBase64(file)
+        setPreview(prev => ({ ...prev, thumb: base64Thumb }))
+    }
+
+
+    //bug ko show len duoc 2 hinh
+    const handlePreviewImages = async (files) => {
+        const imagesPreview = []
+        for (let file of files) {
+            // console.log(file.type)
+            if (file.type !== 'image/png' && file.type !== 'image/jpeg') {
+                toast.warning('This file is not supported')
+                return
+            }
+            const base64 = await getBase64(file)
+            imagesPreview.push({ name: file.name, path: base64 })
+        }
+        setPreview(prev => ({ ...prev, images: imagesPreview }))
+
+    }
+
+    useEffect(() => {
+        reset({
+            title: editProduct?.title || '',
+            price: editProduct?.price || '',
+            quantity: editProduct?.quantity || '',
+            color: editProduct?.color || '',
+            category: editProduct?.category || '',
+            brand: editProduct?.brand?.toLowerCase() || '',
+        })
+        setPayload({
+            description: typeof editProduct?.description === 'object'
+                ? editProduct?.description?.join(',')
+                : editProduct?.description
+        })
+        setPreview({
+            thumb: editProduct?.thumb || '',
+            images: editProduct?.images || []
+        })
+    }, [editProduct])
+
+    useEffect(() => {
+        if (watch('thumb'))
+            handlePreviewThumb(watch('thumb')[0])
+    }, [watch('thumb')])
+
+    useEffect(() => {
+        if (watch('images'))
+            handlePreviewImages(watch('images'))
+    }, [watch('images')])
+
+    const handleCreateProduct = async (data) => {
+        const invalids = validate(payload, setInvalidFields)
+        if (invalids === 0) {
+            if (data.category) data.category = categories?.find(el => el._id === data.category)?.title
+            const finalPayload = { ...data, ...payload }
+            const formData = new FormData()
+            for (let i of Object.entries(finalPayload)) formData.append(i[0], i[1])
+
+            if (finalPayload.thumb) formData.append('thumb', finalPayload.thumb[0])
+            if (finalPayload.images) {
+                for (let image of finalPayload.images) formData.append('images', image)
+            }
+
+            // Display the key/value pairs
+            // for (var pair of formData.entries()) {
+            //     console.log(pair[0] + ', ' + pair[1]);
+            // }
+            dispath(showModel({ isShowModel: true, modelChildren: <Loading /> }))
+            const response = await apiCreateProduct(formData)
+            dispath(showModel({ isShowModel: false, modelChildren: null }))
+            if (response.success) {
+                toast.success(response.mes)
+                reset()
+                setPayload({
+                    thumb: '',
+                    images: []
+                })
+            } else toast.error(response.mes)
+        }
+    }
+    return (
+        <div className='w-full flex flex-col gap-4 relative'>
+            <div className='h-[70px] w-full'></div>
+            <div className='bg-gray-100 p-4 border-b w-full justify-between items-center border-gray-900 fixed'>
+                <h1 className='text-3xl font-bold tracking-tight'>Update Product</h1>
+            </div>
+
+            <div className='p-4'>
+                <form
+                    onSubmit={handleSubmit(handleCreateProduct)}
+                >
+                    <InputForm
+                        label='Name Product'
+                        register={register}
+                        errors={errors}
+                        id='title'
+                        validate={{
+                            required: 'Need fill this field'
+                        }}
+                        fullWidth
+                        placeholder='Enter name of new Product...'
+                    />
+                    <div className='w-full flex gap-4 my-6'>
+                        <InputForm
+                            label='Price'
+                            register={register}
+                            errors={errors}
+                            id='price'
+                            validate={{
+                                required: 'Need fill this field'
+                            }}
+                            fullWidth
+                            placeholder='Enter price of new Product...'
+                            type='number'
+                            style='flex-auto'
+
+                        />
+
+                        <InputForm
+                            label='Quantity'
+                            register={register}
+                            errors={errors}
+                            id='price'
+                            validate={{
+                                required: 'Need fill this field'
+                            }}
+                            fullWidth
+                            placeholder='Enter price of new Product...'
+                            type='number'
+                            style='flex-auto'
+                        />
+
+                        <InputForm
+                            label='Color'
+                            register={register}
+                            errors={errors}
+                            id='color'
+                            validate={{
+                                required: 'Need fill this field'
+                            }}
+                            fullWidth
+                            placeholder='Enter color of new Product...'
+                            style='flex-auto'
+                        />
+                    </div>
+                    <div className='w-full flex gap-4 my-6'>
+                        <Select
+                            label='Category'
+                            options={categories?.map(el => ({ code: el.title, value: el.title }))}
+                            register={register}
+                            id='category'
+                            validate={{
+                                required: 'Need fill this field'
+                            }}
+                            style='flex-auto'
+                            errors={errors}
+                            fullWidth
+                        />
+
+                        <Select
+                            label='Brand (Optional)'
+                            options={categories?.find(el => el.title === watch('category'))?.brand?.map(el => ({ code: el.toLowerCase(), value: el }))}
+                            register={register}
+                            id='brand'
+                            style='flex-auto'
+                            errors={errors}
+                            fullWidth
+                        />
+                    </div>
+                    <MarkdownEditor
+                        name='description'
+                        changeValue={changeValue}
+                        label='Description'
+                        invalidFields={invalidFields}
+                        setInvalidFields={setInvalidFields}
+                        value={payload.description}
+                    />
+                    <div className='flex flex-col gap-2 '>
+                        <label className='font-semibold mt-8' htmlFor='thumb'>Upload thumb</label>
+                        <input
+                            type='file'
+                            id='thumb'
+                            {...register('thumb', { required: 'Need fill this field' })}
+                        />
+                        {errors['thumb'] && <small
+                            className='text-xs text-main'>
+                            {errors['thumb']?.message}
+                        </small>}
+                    </div>
+
+                    {preview.thumb && <div className='my-4'>
+                        <img
+                            src={preview.thumb}
+                            alt='thumbnail'
+                            className='w-[200px] object-contain'
+                        />
+                    </div>}
+
+                    <div className='flex flex-col gap-2 '>
+                        <label className='font-semibold mt-8' htmlFor='product'>Upload images of product</label>
+                        <input
+                            type='file'
+                            id='product'
+                            multiple
+                            {...register('images', { required: 'Need fill this field' })}
+                        />
+                        {errors['images'] && <small
+                            className='text-xs text-main'
+                        >
+                            {errors['images']?.message}
+                        </small>}
+                    </div>
+
+                    {preview.images.length > 0 && <div className='my-4 flex w-full gap-3 flex-wrap'>
+                        {preview.images?.map((el, idx) => (
+                            <div
+                                key={idx}
+                                className='w-fit relative'>
+                                <img
+                                    src={el}
+                                    alt='product'
+                                    className='w-[200px] object-contain'
+                                />
+
+                            </div>
+                        ))}
+                    </div>}
+
+                    <div className='my-8'><Button type='submit'>UPDATE NEW PRODUCT</Button></div>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default memo(UpdateProduct)
