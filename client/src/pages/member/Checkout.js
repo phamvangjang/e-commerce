4d440be019087035bad562bc287efa5dc07336ac
phamvangjang <giangphamvan48@gmail.com>
2024-05-23 17:07:27 +0700

    feat: handle function payment

--- client/src/pages/member/Checkout.js
+++ client/src/pages/member/Checkout.js
@@ -1,9 +1,51 @@
 import React from 'react'
+import payment from '../../assets/payment.svg'
+import { useSelector } from 'react-redux'
+import { formatMoney, formatPrice } from 'ultils/helpers'
+import { Paypal } from 'components'
 
 const Checkout = () => {
+    const { currentCart } = useSelector(state => state.user)
+    console.log(currentCart)
     return (
-        <div>
-            Checkout
+        <div className='w-full p-8 grid grid-cols-10 gap-6 h-full max-h-screen overflow-y-auto'>
+            <div className='w-full flex justify-center items-center col-span-4'>
+                <img
+                    src={payment}
+                    alt='payment'
+                    className='h-[70%] object-contain'
+                />
+            </div>
+
+            <div className='w-full flex flex-col justify-center col-span-6 gap-6 items-center'>
+                <h2 className='mb-6 text-3xl font-bold'>Checkout your order</h2>
+                <table className='table-auto w-full'>
+                    <thead>
+                        <tr className='border bg-gray-200'>
+                            <th className='text-start p-2'>Product</th>
+                            <th className='text-center p-2'>Quantity</th>
+                            <th className='text-end p-2'>Price</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {currentCart?.map(el => (<tr className='border' key={el._id}>
+                            <td className='text-start p-2'>{el?.title}</td>
+                            <td className='text-center p-2'>{el?.quantity}</td>
+                            <td className='text-end p-2'>{`${formatMoney(formatPrice(el?.price))} VND`}</td>
+                        </tr>))}
+                    </tbody>
+                </table>
+                <span className='flex items-center gap-8 text-sm'>
+                    <span>Subtotal: </span>
+                    <span className='text-main font-bold'>{`${formatMoney(currentCart?.reduce((sum, el) => +el?.price * el?.quantity + sum, 0))} VND`}</span>
+                </span>
+                <div>
+                    input address
+                </div>
+                <div className='w-full'>
+                    <Paypal amount={12000000} />
+                </div>
+            </div>
         </div>
     )
 }
