a412b86afc2443b83508ab9c36e82a48b4e3bb7e
phamvangjang <giangphamvan48@gmail.com>
2024-10-27 20:49:43 +0700

    feat: brands

--- client/src/components/inputs/Select.js
+++ client/src/components/inputs/Select.js
@@ -6,14 +6,13 @@ import { useForm } from 'react-hook-form'
 const Select = ({
     label,
     options = [],
-    // register,
-    // errors,
+    register,
+    errors,
     id,
     validate,
     style,
     fullWidth,
     defaultValue }) => {
-    const { register, formState : { errors } } = useForm()
     return (
         <div className={clsx('flex flex-col gap-2', style)}>
             {label && <label htmlFor={id} >{label}</label>}
@@ -23,23 +22,13 @@ const Select = ({
                 id={id}
                 {...register(id, validate)}
             >
-                <option
-                    value=""
-                >CHOOSE</option>
+                <option value="">----CHOOSE----</option>
                 {options?.map(el => (
-                    <option
-                        className='border-none p-2'
-                        value={el.code}>
-                        {el.value}
-                    </option>
+                    <option className='border-none p-2' value={el?.code}>{el?.value}</option>
                 ))}
             </select>
-            {
-                errors[id] && <small
-                    className='text-xs text-main'
-                >{errors[id]?.message}</small>
-            }
-        </div >
+            {errors[id] && <small className='text-xs text-main'>{errors[id]?.message}</small>}
+        </div>
     )
 }
 
